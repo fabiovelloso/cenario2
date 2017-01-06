26dc8fa559c4ea45f8068b2e7c9fa55ea800eeb7
Fabio Velloso <git-tower@fabiovelloso.com.br>
2017-01-05 22:32:49 -0200

    first commit

---
+++ init.sql
@@ -0,0 +1,8 @@
+create user ORIGINAL identified by ORIGINAL;
+grant all privileges to ORIGINAL;
+CREATE TABLE "ORIGINAL"."GERENTES"("ID" NUMBER,"NOME" NVARCHAR2(30)) ;
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('1’,'Joao');
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('2’,'Maria');
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('3’,'Joaquim');
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('4’,'Manoel');
+
