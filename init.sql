8b007b2a5615e268346160ec7a09dc020ec77a4d
Fabio Velloso <git-tower@fabiovelloso.com.br>
2017-01-06 16:23:39 -0200

    Alteracao data Vanessa

--- init.sql
+++ init.sql
@@ -4,5 +4,5 @@ CREATE TABLE "ORIGINAL"."GERENTES"("ID" NUMBER,"NOME" NVARCHAR2(30)) ;
 Insert into ORIGINAL.GERENTES (ID,NOME) values ('1','Joao');
 Insert into ORIGINAL.GERENTES (ID,NOME) values ('2','Maria');
 Insert into ORIGINAL.GERENTES (ID,NOME) values ('3','Joaquim');
-Insert into ORIGINAL.GERENTES (ID,NOME) values ('4','Manoel');
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('4','Zequinha');
 commit;
