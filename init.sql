9a30474b46c422e48adb7af6dbffff4c1ed49f92
Fabio Velloso <git-tower@fabiovelloso.com.br>
2017-01-05 22:37:47 -0200

    ajuste 2

--- init.sql
+++ init.sql
@@ -1,8 +1,7 @@
 create user ORIGINAL identified by ORIGINAL;
 grant all privileges to ORIGINAL;
 CREATE TABLE "ORIGINAL"."GERENTES"("ID" NUMBER,"NOME" NVARCHAR2(30)) ;
-Insert into ORIGINAL.GERENTES (ID,NOME) values ('1’,'Joao');
-Insert into ORIGINAL.GERENTES (ID,NOME) values ('2’,'Maria');
-Insert into ORIGINAL.GERENTES (ID,NOME) values ('3’,'Joaquim');
-Insert into ORIGINAL.GERENTES (ID,NOME) values ('4’,'Manoel');
-
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('1','Joao');
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('2','Maria');
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('3','Joaquim');
+Insert into ORIGINAL.GERENTES (ID,NOME) values ('4','Manoel');
